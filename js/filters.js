a4bf19bf62be40be8048d91733f3f6efcabdf130
justin529 <118300903+justin529@users.noreply.github.com>
2026-04-16 21:29:53 +0200

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -8,19 +8,18 @@ function buildTable(data) {
     const tbody = document.querySelector("#medewerkersTable tbody");
     tbody.innerHTML = "";
 
-    data.forEach(medewerker => {
+    data.forEach(m => {
+        const statusText = m.status?.actief ? "Actief" : "Inactief";
+
+        const geslaagd = countStatus(m, "geslaagd");
+        const open = countStatus(m, "open");
+        const gezakt = countStatus(m, "gezakt");
+
         const tr = document.createElement("tr");
-
-        const statusText = medewerker.status?.actief ? "Actief" : "Inactief";
-
-        const geslaagd = countStatus(medewerker, "geslaagd");
-        const open = countStatus(medewerker, "open");
-        const gezakt = countStatus(medewerker, "gezakt");
-
         tr.innerHTML = `
-            <td>${medewerker.id}</td>
-            <td>${medewerker.rang}</td>
-            <td>${medewerker.naam}</td>
+            <td>${m.roepnummer || "-"}</td>
+            <td>${m.rang}</td>
+            <td>${m.naam}</td>
             <td class="status-${statusText.toLowerCase()}">${statusText}</td>
             <td>${geslaagd}</td>
             <td>${open}</td>
@@ -29,19 +28,17 @@ function buildTable(data) {
         `;
 
         tr.onclick = () => {
-            window.location.href = `medewerker.html?id=${medewerker.id}`;
+            window.location.href = `medewerker.html?id=${m.id}`;
         };
 
         tbody.appendChild(tr);
     });
 }
 
-function countStatus(medewerker, type) {
+function countStatus(m, type) {
     let count = 0;
-    for (const training of Object.values(medewerker.trainingen || {})) {
-        if (training.praktijk === type || training.theorie === type) {
-            count++;
-        }
+    for (const t of Object.values(m.trainingen || {})) {
+        if (t.praktijk === type || t.theorie === type) count++;
     }
     return count;
 }
